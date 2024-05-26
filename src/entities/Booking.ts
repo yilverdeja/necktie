d806658b277e5f81ee35b4b7de85c0722d21f6ec
Yil Verdeja <yilverdeja@gmail.com>
2024-05-26 20:30:30 +0800

    feat: add models
    added booking & doctor models

---
+++ src/entities/Booking.ts
@@ -0,0 +1,8 @@
+export default interface Booking {
+	id: string;
+	name: string;
+	start: number;
+	doctorId: string;
+	date: Date;
+	status: 'cancel' | 'confirmed';
+}
---
+++ src/entities/Doctor.ts
@@ -0,0 +1,20 @@
+interface DoctorAddress {
+	line_1: string;
+	line_2: string;
+	district: string;
+}
+
+interface OpeningHours {
+	start: string;
+	end: string;
+	isClosed: boolean;
+	day: 'MON' | 'TUE' | 'WED' | 'THU' | 'FRI' | 'SAT' | 'SUN';
+}
+
+export default interface Doctor {
+	id: string;
+	name: string;
+	description: string;
+	address: DoctorAddress;
+	opening_hours: OpeningHours[];
+}
