17a9cee1e5fac93bfd854476e9927e45056eabb1
Yil Verdeja <yilverdeja@gmail.com>
2024-05-27 18:25:03 +0800

    feat: add navbar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import { Toaster } from '@/components/ui/toaster';
+import Navbar from '@/components/Navbar';
 const Layout = () => {
 	return (
 		<>
+			<Navbar />
 			<div className="max-w-6xl mx-auto p-4 lg:p-8">
 				<Outlet />
 			</div>
